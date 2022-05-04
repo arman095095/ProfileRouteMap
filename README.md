986f8de4426c9855eef07fd4cc991a2e4cd58c5e
Арман Чархчян <re-mont1@mail.ru>
2022-05-04 21:55:33 +0400

    initital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProfileRouteMap
+
+A description of this package.
---
+++ Sources/ProfileRouteMap/ProfileRouteMap.swift
@@ -0,0 +1,18 @@
+
+import Module
+import Managers
+
+public protocol ProfileRouteMap: AnyObject {
+    func currentAccountModule(profile: ProfileModelProtocol) -> ProfileModule
+    func friendAccountModule(profile: ProfileModelProtocol) -> ProfileModule
+}
+
+public protocol ProfileModuleInput: AnyObject {
+    
+}
+
+public protocol ProfileModuleOutput: AnyObject {
+    func openAccountSettingsModule()
+}
+
+public typealias ProfileModule = Module<ProfileModuleInput, ProfileModuleOutput>
