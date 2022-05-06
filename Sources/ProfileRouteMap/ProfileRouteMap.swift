06ab6ac2542fb27ee9e3ef032e07493e05e4f4db
Арман Чархчян <re-mont1@mail.ru>
2022-05-06 11:22:46 +0400

    fix

--- Sources/ProfileRouteMap/ProfileRouteMap.swift
+++ Sources/ProfileRouteMap/ProfileRouteMap.swift
@@ -5,6 +5,9 @@ import ModelInterfaces
 public protocol ProfileRouteMap: AnyObject {
     func currentAccountModule(profile: ProfileModelProtocol) -> ProfileModule
     func friendAccountModule(profile: ProfileModelProtocol) -> ProfileModule
+    func offerSendProfile(profile: ProfileModelProtocol) -> ProfileModule
+    func offerRecieveProfile(profile: ProfileModelProtocol) -> ProfileModule
+    func offersSendingProfilesList() -> ProfileModule
 }
 
 public protocol ProfileModuleInput: AnyObject {
